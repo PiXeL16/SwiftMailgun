432d987b70bf0471b9b7ae0dba5bb8dbe4c1c6dd
Chris Jimenez <cjimenez16@gmail.com>
2016-03-08 01:23:53 -0600

    Update test

--- SwiftMailgunTests/MailgunApiSpecs.swift
+++ SwiftMailgunTests/MailgunApiSpecs.swift
@@ -57,65 +57,6 @@ class MailgunApiSpecs: QuickSpec{
             expect(result?.message).toEventuallyNot(beNil(), timeout:timeout)
             
         }
-//
-//        it("send email with inline parameters"){
-//            
-//            let keys = SwiftmandrillKeys()
-//            
-//            let api = MandrillAPI(ApiKey: keys.mANDRILL_API())
-//            
-//            var result : MandrillResult? = nil
-//            
-//            api.sendEmail(from: "test@test.com", fromName: "Test", to: "cjimenez16@gmail.com", subject: "test", html: "test", text: "test"){ mandrillResult in
-//                result = mandrillResult
-//            }
-//            
-//            expect(result).toEventuallyNot(beNil(),timeout:timeout)
-//            expect(result!.hasError).toEventually(beFalsy(),timeout:timeout)
-//            expect(result!.message).toEventually(beNil(), timeout:timeout)
-//            
-//        }
-//        
-//        it("send email with inline parameters to several recipients"){
-//            
-//            let keys = SwiftmandrillKeys()
-//            
-//            let api = MandrillAPI(ApiKey: keys.mANDRILL_API())
-//            
-//            var result : MandrillResult? = nil
-//            
-//            api.sendEmail(from: "test@test.com",fromName: "test", to: ["cjimenez16@gmail.com","test2@test.com"], subject: "test", html: "test", text: "test"){ mandrillResult in
-//                result = mandrillResult
-//            }
-//            
-//            expect(result).toEventuallyNot(beNil(),timeout:timeout)
-//            expect(result!.hasError).toEventually(beFalsy(),timeout:timeout)
-//            expect(result!.message).toEventually(beNil(), timeout:timeout)
-//            
-//        }
-//        
-//        it("send email to several recipients with email object"){
-//            
-//            let keys = SwiftmandrillKeys()
-//            
-//            let api = MandrillAPI(ApiKey: keys.mANDRILL_API())
-//            
-//            var result : MandrillResult? = nil
-//            
-//            let to1 = MandrillTo(email: "test@test.com")
-//            let to2 = MandrillTo(email: "test@test2.com")
-//            
-//            let email = MandrillEmail(from: "test@test.com", fromName: "test", to: [to1,to2], subject: "test", html: "test", text: "test")
-//            
-//            api.sendEmail(withEmail: email){ mandrillResult in
-//                result = mandrillResult
-//            }
-//            
-//            expect(result).toEventuallyNot(beNil(),timeout:timeout)
-//            expect(result!.hasError).toEventually(beFalsy(),timeout:timeout)
-//            expect(result!.message).toEventually(beNil(), timeout:timeout)
-//            
-//        }
         
     }
 
